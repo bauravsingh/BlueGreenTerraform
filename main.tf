373d8a99bc397202f52a2b5a467e98a7867fb4a8
Baurav Singh <103564761+bauravsingh@users.noreply.github.com>
2023-03-29 16:42:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 locals {
-  vpc_id              = "vpc-05d651cd79f658e69"
-  private_a_subnet_id = "subnet-0881a29fbac709de1"
-  private_b_subnet_id = "subnet-0d9007e51e3d3daeb"
-  public_a_subnet_id  = "subnet-07da1b75fed56ff1d"
-  public_b_subnet_id  = "subnet-01fbca6a288242d24"
+  vpc_id              = "vpc-087f470130cb637c9"
+  private_a_subnet_id = "subnet-0188fcda7c577954d"
+  private_b_subnet_id = "subnet-00d7b4b4a2169fb3c"
+  public_a_subnet_id  = "subnet-080ed0a8cbd7d9278"
+  public_b_subnet_id  = "subnet-0588a0afbf33ca630"
 
   ubuntu_ami = "ami-0f8ca728008ff5af4"
 
